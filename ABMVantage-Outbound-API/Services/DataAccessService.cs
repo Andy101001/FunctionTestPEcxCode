2b1021ffef68d821bbbb6c70814b32aaba12f098
Jason Green <Jason.Green@abm.com>
2023-02-25 20:52:36 -0500

    added the occupancy function.

--- ABMVantage-Outbound-API/Services/DataAccessService.cs
+++ ABMVantage-Outbound-API/Services/DataAccessService.cs
@@ -19,13 +19,13 @@
         /// Initializes a new instance of the <see cref="DashboardService"/> class.
         /// </summary>
         /// <param name="factory">The factory instance.</param>
-        public DataAccessService(IDbContextFactory<CosmosDataContext> factory) =>_factory = factory;
+        public DataAccessService(IDbContextFactory<CosmosDataContext> factory) => _factory = factory;
 
         /// <summary>
         /// Get active charging sessions
         /// </summary>
         /// <param name="id">optional id for a session</param>
-        /// <returns>List<EvActiveSessions></returns>        
+        /// <returns>List<EvActiveSessions></returns>
         public async Task<List<EvActiveSessions>>? GetActiveChargingSessionsAsync(string? id = null)
         {
             using var context = _factory.CreateDbContext();
@@ -39,14 +39,28 @@
         /// Get closed charging sessions
         /// </summary>
         /// <param name="id">Optional Id for Closed Sessions</param>
-        /// <returns>List<EvClosedSessions></returns>        
+        /// <returns>List<EvClosedSessions></returns>
         public async Task<List<EvClosedSessions>>? GetClosedChargingSessionsAsync(string? id = null)
         {
             using var context = _factory.CreateDbContext();
 
             var evClosedSessions = await context.EvClosedSessions.ToListAsync();
 
-            return evClosedSessions; 
+            return evClosedSessions;
+        }
+
+        /// <summary>
+        /// Get Parcs Ticket Occupancies
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns>List<Occupancy></returns>
+        public async Task<List<Occupancy>>? GetParcsTicketOccupanciesAsync(string? id = null)
+        {
+            using var context = _factory.CreateDbContext();
+
+            var parcsTicketOccupancies = await context.ParcsTickOccupanies.ToListAsync();
+
+            return parcsTicketOccupancies;
         }
 
         /// <summary>
@@ -65,15 +79,19 @@
             return reservation;
         }
 
+        /// <summary>
+        /// Get Reservation Transactions
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>List<ObsReservationTransactions></returns>
         public async Task<List<ObsReservationTransactions>>? GetReservationsTransactionsAsync(string? id = null)
         {
             using var context = _factory.CreateDbContext();
 
             var obsReservationTransactions = await context.ReservationTransactions.ToListAsync();
             // Call a service method to do this biz for getting the transactions for the reservation
-            
 
-            return new List<ObsReservationTransactions> {new ObsReservationTransactions() };
+            return new List<ObsReservationTransactions> { new ObsReservationTransactions() };
         }
     }
 }
