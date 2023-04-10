040801131fbe37dcc6e1cce20195acbd8090e5d2
David Somuah <David.Somuah@abm.com>
2023-04-09 22:43:15 -0400

    Create swagger documentation for daily average occupancy function.

---
+++ ABMVantage-Outbound-API/DashboardFunctionModels/DashboardDailyAverageOccupancy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMVantage_Outbound_API.DashboardFunctionModels
+{
+    public class DashboardDailyAverageOccupancy
+    {
+        public int AverageDailyOccupancyInteger { get; set; }
+        public int AverageDailyOccupancyPercentage { get; set; }
+    }
+}
