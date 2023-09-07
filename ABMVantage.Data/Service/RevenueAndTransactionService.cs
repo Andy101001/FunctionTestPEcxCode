9d6e89652ab2e616d399e0bea239c0de1768715f
Charan Verma <Charan.Verma@abm.com>
2023-09-07 14:23:31 -0400

    Revert back % to $ value Bug # 5710

--- ABMVantage.Data/Service/RevenueAndTransactionService.cs
+++ ABMVantage.Data/Service/RevenueAndTransactionService.cs
@@ -170,9 +170,9 @@
                        {
                            FirstDayOfMonth = g.Key,
                            Month = g.Key.ToString("MMM"),
-                           //BgtVariance = g.Sum(x => x.BudgetedRevenue) > 0 ? ((g.Sum(x => x.Revenue) - g.Sum(x => x.BudgetedRevenue)) / g.Sum(x => x.BudgetedRevenue)) : 0,
+                           BgtVariance = g.Sum(x => x.BudgetedRevenue) > 0 ? ((g.Sum(x => x.Revenue) - g.Sum(x => x.BudgetedRevenue)) / g.Sum(x => x.BudgetedRevenue)) : 0,
                            //Bug # 5710 desc: Budget Vs Actual Variance Card shows percentage when it should show a number
-                           BgtVariance = g.Sum(x => x.BudgetedRevenue) > 0 ? ((g.Sum(x => x.Revenue) - g.Sum(x => x.BudgetedRevenue))) : 0,
+                           //BgtVariance = g.Sum(x => x.BudgetedRevenue) > 0 ? ((g.Sum(x => x.Revenue) - g.Sum(x => x.BudgetedRevenue))) : 0,
                        }
                        ).OrderBy(x => x.FirstDayOfMonth).ToArray();
 
